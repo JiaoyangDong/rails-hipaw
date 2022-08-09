c832ca72983da0efa06ef2f16577ea4f8ac491c3
zora dong <zoradongccc@gmail.com>
2022-08-09 11:26:59 +0800

    add authentification & add pets#index contrroller

--- app/controllers/api/v1/base_controller.rb
+++ app/controllers/api/v1/base_controller.rb
@@ -2,4 +2,33 @@ class Api::V1::BaseController < ActionController::Base
   HMAC_SECRET = Rails.application.credentials.dig(:jwt, :hmac_secret) # find the secret
 
   skip_before_action :verify_authenticity_token
+  before_action :verify_request
+
+  rescue_from JWT::ExpiredSignature, with: :render_unauthorized
+
+  private
+
+  def verify_request
+    token = find_jwt_token
+    if token.present?
+      data = jwt_decode(token)
+      user_id = data[:user_id]
+      @current_user = User.find(user_id) # set current user by user_id in JWT payload
+    else
+      render json: { error: 'Missing JWT token.' }, status: 401
+    end
+  end
+
+  def jwt_decode(token) # decode JWT, then turn payload into a hash
+    decoded_info = JWT.decode(token, HMAC_SECRET, { algorithm: 'HS256' })[0] # extract the payload
+    HashWithIndifferentAccess.new decoded_info
+  end
+
+  def find_jwt_token # retrieve token from headers
+    request.headers['X-USER-TOKEN']
+  end
+
+  def render_unauthorized
+    render json: { error: 'token expired' }, status: 401
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   # root "articles#index"
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
+      resources :pets
       post 'login', to: 'users#login', as: :login
     end
   end
