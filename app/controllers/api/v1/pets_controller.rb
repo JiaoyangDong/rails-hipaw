df658bb293f058068d5a1c143cd039b21403635c
Ann Altantulga <a3jargal@icloud.com>
2022-08-09 14:07:21 +0800

    pets-create-backend

--- app/controllers/api/v1/pets_controller.rb
+++ app/controllers/api/v1/pets_controller.rb
@@ -3,4 +3,23 @@ class Api::V1::PetsController < ApplicationController
     @pets = Pet.all
     render json: @pets
   end
+
+  def create
+    @pet = Pet.new(pet_params)
+    if @pet.save
+      render :show, status: :created
+    else
+      render error
+    end
+  end
+
+  private
+
+  def pet_params
+    params.require(:pet).permit(:name, :age, :species, :description, :sex, :fur_type, :image_url)
+  end
+
+  def render_error
+    render json: { errors: @pet.errors.full_messages }, status: :unprocessable_entity
+  end
 end
