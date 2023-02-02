53b5fd8f3411639073a70dc4940349b34e030197
Ann Altantulga <a3jargal@icloud.com>
2023-02-02 13:03:30 +0800

    dropped applications table as we are using bookings

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_01_090445) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_02_050139) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -40,15 +40,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_090445) do
     t.bigint "blob_id", null: false
     t.string "variation_digest", null: false
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
-  end
-
-  create_table "applications", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.date "application_date"
-    t.string "application_status"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_applications_on_user_id"
   end
 
   create_table "bookings", force: :cascade do |t|
@@ -87,7 +78,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_01_090445) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
-  add_foreign_key "applications", "users"
   add_foreign_key "bookings", "pets"
   add_foreign_key "bookings", "users"
 end
