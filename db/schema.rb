b9830967b45a46c5d3c82dafba77bb7aa96ed288
annieww <annie9016@hotmail.com>
2023-02-02 14:33:12 +0800

    fixe app.json file

--- db/schema.rb
+++ db/schema.rb
@@ -73,7 +73,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_02_050139) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "open_id"
-    t.boolean "admin"
+    t.string "role"
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
